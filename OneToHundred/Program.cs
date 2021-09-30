48e61b15f785545a7919f10e1908eaa9a4ea38a8
Opilane TTHK <opilane@tthk.ee>
2021-09-30 11:28:25 +0300

    first commit

---
+++ OneToHundred/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace OneToHundred
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm kuvab konsoolis
+            //kasutame for-loopi
+
+            for(int i = 1; i < 101; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+
+        }
+    }
+}
